9cd9824fdea6867a90e6562b111b7d6bd6d0ba10
Tugsbayar Galbadrakh <tugs1ga@gmail.com>
2026-05-05 10:41:31 +0200

    11 commit så perfekt

--- script.js
+++ script.js
@@ -1,147 +1,62 @@
 var varukorg = [];
 
-// sparad varukorg
 var sparad = localStorage.getItem("varukorg");
 if (sparad != null) {
     varukorg = JSON.parse(sparad);
 }
 
-// antal
 function uppdateraAntal() {
     var antalEl = document.getElementById("varukorg-antal");
     if (antalEl == null) return;
-
     var totalt = 0;
-    var i = 0;
-    for (i = 0; i < varukorg.length; i++) {
+    for (var i = 0; i < varukorg.length; i++) {
         totalt = totalt + varukorg[i].antal;
     }
     antalEl.textContent = totalt;
 }
 
-// lägg till vara
 function laggTill(namn, pris) {
     var hittad = false;
-    var i = 0;
-
-    // kolla om finns
-    for (i = 0; i < varukorg.length; i++) {
+    for (var i = 0; i < varukorg.length; i++) {
         if (varukorg[i].namn == namn) {
             varukorg[i].antal = varukorg[i].antal + 1;
             hittad = true;
             break;
         }
     }
-
-    // funkar
     if (hittad == false) {
-        var nyVara = { namn: namn, pris: pris, antal: 1 };
-        varukorg.push(nyVara);
+        varukorg.push({ namn: namn, pris: pris, antal: 1 });
     }
-
     localStorage.setItem("varukorg", JSON.stringify(varukorg));
     uppdateraAntal();
 }
 
-// ändra antal
-function andraAntal(namn, andring) {
-    var i = 0;
-    for (i = 0; i < varukorg.length; i++) {
-        if (varukorg[i].namn == namn) {
-            varukorg[i].antal = varukorg[i].antal + andring;
-
-            if (varukorg[i].antal <= 0) {
-                taBort(namn);
-                return;
-            }
-            break;
-        }
-    }
-    localStorage.setItem("varukorg", JSON.stringify(varukorg));
-    visaVarukorg();
-}
-
-// ta bort vara
-function taBort(namn) {
-    var nyArray = [];
-    var i = 0;
-    for (i = 0; i < varukorg.length; i++) {
-
-        if (varukorg[i].namn != namn) {
-            nyArray.push(varukorg[i]);
-        }
-    }
-    varukorg = nyArray;
-    localStorage.setItem("varukorg", JSON.stringify(varukorg));
-    visaVarukorg();
-}
-
-// töm varukorg
-function tomVarukorg() {
-    varukorg = [];
-    localStorage.setItem("varukorg", JSON.stringify(varukorg));
-    visaVarukorg();
-}
-
-// visa varukorg
 function visaVarukorg() {
     var varorEl = document.getElementById("varukorg-varor");
-    if (varorEl == null) return; // funkar inte på index.html så avbryter
+    if (varorEl == null) return;
 
     uppdateraAntal();
-
-    // totalpris
-    var totalPris = 0;
-    var i = 0;
-    for (i = 0; i < varukorg.length; i++) {
-        totalPris = totalPris + (varukorg[i].pris * varukorg[i].antal);
-    }
-    document.getElementById("total-pris").textContent = totalPris.toLocaleString("sv-SE") + " kr";
-
     varorEl.innerHTML = "";
 
     if (varukorg.length == 0) {
-        var tom = document.createElement("p");
-        tom.textContent = "Din varukorg är tom.";
-        tom.style.color = "#8890A4";
-        varorEl.appendChild(tom);
+        varorEl.innerHTML = "<li>Din varukorg är tom.</li>";
         return;
     }
 
-    // bygg rader
-    for (i = 0; i < varukorg.length; i++) {
-        var vara = varukorg[i];
-
-        var rad = document.createElement("div");
-        rad.className = "varukorg-rad";
-
-        rad.innerHTML =
-            "<p>" + vara.namn + "</p>" +
-            "<span>" + (vara.pris * vara.antal).toLocaleString("sv-SE") + " kr</span>" +
-            "<div class='antal-kontroll'>" +
-                "<button onclick=\"andraAntal('" + vara.namn + "', -1)\">−</button>" +
-                "<span>" + vara.antal + "</span>" +
-                "<button onclick=\"andraAntal('" + vara.namn + "', 1)\">+</button>" +
-            "</div>" +
-            "<button class='ta-bort' onclick=\"taBort('" + vara.namn + "')\">✕</button>";
-
-        varorEl.appendChild(rad);
+    for (var i = 0; i < varukorg.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = varukorg[i].namn + " x" + varukorg[i].antal;
+        varorEl.appendChild(li);
     }
 }
 
-// kassa
 function kassa() {
     if (varukorg.length == 0) return;
-
-    var total = 0;
-    var i = 0;
-    for (i = 0; i < varukorg.length; i++) {
-        total = total + (varukorg[i].pris * varukorg[i].antal);
-    }
-    alert("Beställning på " + total.toLocaleString("sv-SE") + " kr genomförd!");
-    tomVarukorg();
+    varukorg = [];
+    localStorage.setItem("varukorg", JSON.stringify(varukorg));
+    visaVarukorg();
+    alert("Tack för din beställning!");
 }
 
-// starta
 uppdateraAntal();
 visaVarukorg();
